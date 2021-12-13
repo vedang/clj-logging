f8fc37344011b761e8c2801e37a8477a78c7906c
Vedang Manerikar <ved.manerikar@gmail.com>
2021-12-13 22:51:13 +0530

    Initial Commit: Dummy project.clj file for setting up Log4J2
    1. Add all the necessary artifacts for using log4j2 as the primary
    logging backend.
    
    2. Add SLF4J as the primary logging facade
    
    3. Redirect all logging backends to bridge over SLF4J, so that we can
    control the actual logging backend used
    
    4. Explicitly specify the exclusions we need to set in each Clojure
    project.
    
    5. Tell clojure.tools.logging to use Log4J2 as it's logging factory.
    
    6. Tell Jetty to use SLF4J as a logging bridge.
    
    7. Use LMAX Disruptor and AsyncLoggerContextSelectors for the fastest
    possible logging experience.
    
    References:
    - https://github.com/stuartsierra/log.dev
    - https://www.loggly.com/blog/benchmarking-java-logging-frameworks/
    - https://logging.apache.org/log4j/2.x/manual/async.html
    - https://krishankantsinghal.medium.com/logback-slf4j-log4j2-understanding-them-and-learn-how-to-use-d33deedd0c46

---
+++ project.clj
@@ -0,0 +1,55 @@
+(defproject me.vedang/clj-logging "1.0.0-SNAPSHOT"
+  :description "Dummy to set up Log4J correctly for Clojure projects"
+  :url "https://github.com/vedang/clj-logging"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]]}}
+  :dependencies [;; Set up Log4J2 as the logging backend with proper
+                 ;; log library conflict resolution.
+
+                 ;; Use log4j2 as the primary logging library
+                 [org.apache.logging.log4j/log4j-api "2.15.0"]
+                 [org.apache.logging.log4j/log4j-core "2.15.0"]
+                 ;; Needed for logging from inside javax.servlet
+                 [org.apache.logging.log4j/log4j-web "2.15.0"]
+
+                 ;; Use clojure.tools.logging as the primary clojure facade
+                 [org.clojure/tools.logging "1.2.1"]
+
+                 ;; Use SLF4j as the primary facade for gathering all logs.
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.15.0"]
+                 [org.slf4j/slf4j-api "1.7.32"]
+
+                 ;; Use LMAX Disruptor to enable Async Logging
+                 [com.lmax/disruptor "3.4.4"]
+
+                 ;; Redirect everything via the SLF4J API
+
+                 ;; Apache Commons logging
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 ;; Log4j 1.x
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]
+                 ;; OSGI LogService
+                 [org.slf4j/osgi-over-slf4j "1.7.32"]
+                 ;; Java util logging
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+
+                 ;; DONE
+                 ]
+
+  :exclusions
+  [;; Exclude transitive dependencies on all other logging
+   ;; implementations, including other SLF4J bridges.
+   commons-logging
+   log4j ;; 1.x
+   ch.qos.logback/logback-classic
+   ch.qos.logback/logback-core
+   org.slf4j/simple
+   org.slf4j/slf4j-jcl
+   org.slf4j/slf4j-nop
+   org.slf4j/slf4j-log4j12
+   org.slf4j/slf4j-log4j13]
+  :jvm-opts
+  ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/log4j2-factory"
+   "-Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.Slf4jLog"
+   "-Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"])
