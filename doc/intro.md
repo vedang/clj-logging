c7bec369bd1bd417ac1ba117b37f6a8a58a89fa2
Vedang Manerikar <ved.manerikar@gmail.com>
2024-03-22 21:24:00 +0530

    In the beginning there was darkness

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to me.vedang/logger
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/me/vedang/logger_test.clj
@@ -0,0 +1,13 @@
+(ns me.vedang.logger-test
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.test :refer :all]
+            [org.corfield.new] ; for the Specs
+            [me.vedang.logger :refer :all]))
+
+(deftest valid-template-test
+  (testing "template.edn is valid."
+    (let [template (edn/read-string (slurp (io/resource "me/vedang/logger/template.edn")))]
+      (is (s/valid? :org.corfield.new/template template)
+          (s/explain-str :org.corfield.new/template template)))))
